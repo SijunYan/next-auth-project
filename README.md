3a63e5c8325db53a1b7569cfe4b765ca810cf19b
Sijun Yan <yan_sijun@hotmail.com>
2025-03-31 11:21:13 +1100

    add register api call

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ This is a nextjs project for Auth setup demo purpose
 # Tutorial Url
 [Link](https://www.youtube.com/watch?v=1MTyCvS05V4&t=10092s)
 
+# database
+```
+prisma init
+
+# generate client after specifying the schema and db connection
+npx prisma generate
+
+# sync
+npx prisma db push
+```
+
--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,59 @@
 'use server'
 
+import bcrypt from 'bcrypt'
 import { RegisterFormType, RegisterSchema } from "../schemas";
+import { db } from '@/lib/db';
+import { Prisma } from '@prisma/client';
 
 export const register = async(values: RegisterFormType) => {
-    console.log(values)
+    try {
+        console.log(values)
 
-    const validateFields = RegisterSchema.safeParse(values)
+        // Validate data
+        const validateFields = RegisterSchema.safeParse(values)
 
-    if(!validateFields.success) {
-        return { error: 'Ivalid fields!' }
+        if(!validateFields.success) {
+            return { error: 'Ivalid fields!' }
+        }
+
+        const { email, password, name } = validateFields.data
+
+        // Valid email if unique (it can be done by database)
+        // const existingUser = await db.user.findUnique({
+        //     where: { 
+        //         email
+        //     },
+        // })
+        // if (existingUser) {
+        //     return { error: 'Email already in use!'}
+        // }
+
+        // Hash password
+        const hassedPassword = await bcrypt.hash(password, 10)
+
+        await db.user.create({
+            data: {
+                name,
+                email,
+                password: hassedPassword
+            }
+        })
+
+        // TODO: send valification token email
+
+        return { success: 'User created!'}
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            console.log("Known Error: ", error.code, error.meta, error.message)
+            if (error.code === 'P2002') {
+                return { error: 'Email already in use!'}
+            }
+        }
+
+        console.log("Unknown Error: ", error)
+        return { error: 'Unexpected error! please try again'}
     }
+    
 
-    return { success: 'Email sent!'}
+    
 }
